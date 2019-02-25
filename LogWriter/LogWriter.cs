3d52139f77c5590f8c803b28b8672c7113287e40
Gowtham T <vtgowtham1993@gmail.com>
2019-02-25 12:46:42 +0530

    init commit

---
+++ LogWriter/LogWriter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LogWriter
+{
+
+    public enum LogLevel
+    {
+        Info = 0, // Device or user information
+        Action = 1, // If user Click or navigate to something
+        Debug = 2, // Used to debug application to find error.
+        Error = 3, // Error occured in applicatoin
+        Console = 4,
+        All = 5, // all type of information.
+    }
+    public class LogWriter
+    {
+
+        LogLevel _logLevel { get; }
+
+        public LogWriter(LogLevel logLevel)
+        {
+            _logLevel = logLevel;
+        }
+
+        public
+    }
+}
